67abd4466b2edc82318839b51b1e95b82363af28
Agni Advani <advani.agni.agni@gmail.com>
2020-06-13 04:12:02 +0530

    adding benchmarks (#2)

--- 01_mvc/domain/user_dao_test.go
+++ 01_mvc/domain/user_dao_test.go
@@ -3,17 +3,25 @@ package domain
 import (
 	"net/http"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGetUserNotFound(t *testing.T) {
 	user, err := GetUser(0)
-	if user != nil {
-		t.Error("We were not expecting a user with an id 0")
-	}
-	if err == nil {
-		t.Error("We were expecting an error when user id was 0")
-	}
-	if err.Status != http.StatusNotFound {
-		t.Error("We were expecting status 404")
-	}
+	assert.Nil(t, user, "We are expecting user to be nil at id 0")
+	assert.NotNil(t, err, "we are expecting error not to be nil")
+	assert.EqualValues(t, http.StatusNotFound, err.Status)
+	assert.EqualValues(t, "User not found", err.Message)
+	assert.EqualValues(t, "not_found", err.Code)
+}
+
+func TestGetUserNoError(t *testing.T) {
+	user, err := GetUser(123)
+	assert.NotNil(t, user)
+	assert.Nil(t, err)
+	assert.EqualValues(t, user.UserID, 123)
+	assert.EqualValues(t, user.FirstName, "Wayne")
+	assert.EqualValues(t, user.LastName, "Rooney")
+	assert.EqualValues(t, user.Email, "waynerooney@ggmu.com")
 }
---
+++ 01_mvc/utilis/sort_utilis_test.go
@@ -0,0 +1,126 @@
+package utilis
+
+import (
+	"sort"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBubbleSort(t *testing.T) {
+	els := []int{5, 4, 3, 2, 1}
+	BubbleSort(els)
+
+	assert.NotNil(t, els)
+	assert.EqualValues(t, 5, len(els))
+	assert.EqualValues(t, 1, els[0])
+	assert.EqualValues(t, 2, els[1])
+	assert.EqualValues(t, 3, els[2])
+	assert.EqualValues(t, 4, els[3])
+	assert.EqualValues(t, 5, els[4])
+}
+func makeElements(len int) []int {
+	els := make([]int, len)
+	i := 0
+	for j := len - 1; j >= 0; j-- {
+		els[i] = j
+		i++
+	}
+	return els
+}
+func TestMakeElements(t *testing.T) {
+	els := makeElements(5)
+	assert.NotNil(t, els)
+	assert.EqualValues(t, 5, len(els))
+	assert.EqualValues(t, 4, els[0])
+	assert.EqualValues(t, 3, els[1])
+	assert.EqualValues(t, 2, els[2])
+	assert.EqualValues(t, 1, els[3])
+	assert.EqualValues(t, 0, els[4])
+
+}
+
+func BenchmarkBubbleSort100(b *testing.B) {
+
+	els := makeElements(100)
+	for i := 0; i < b.N; i++ {
+		BubbleSort(els)
+	}
+}
+
+func BenchmarkBubbleSort1000(b *testing.B) {
+
+	els := makeElements(1000)
+	for i := 0; i < b.N; i++ {
+		BubbleSort(els)
+	}
+}
+
+func BenchmarkBubbleSort100000(b *testing.B) {
+
+	els := makeElements(100000)
+	for i := 0; i < b.N; i++ {
+		BubbleSort(els)
+	}
+}
+
+func BenchmarkNativeSort100(b *testing.B) {
+	els := makeElements(100)
+	for i := 0; i < b.N; i++ {
+		sort.Ints(els)
+	}
+}
+
+func BenchmarkNativeSort1000(b *testing.B) {
+	els := makeElements(1000)
+	for i := 0; i < b.N; i++ {
+		sort.Ints(els)
+	}
+}
+
+func BenchmarkNativeSort100000(b *testing.B) {
+	els := makeElements(100000)
+	for i := 0; i < b.N; i++ {
+		sort.Ints(els)
+	}
+}
+
+func BenchmarkBubbleSort50000(b *testing.B) {
+
+	els := makeElements(50000)
+	for i := 0; i < b.N; i++ {
+		BubbleSort(els)
+	}
+}
+
+func BenchmarkNativeSort50000(b *testing.B) {
+	els := makeElements(50000)
+	for i := 0; i < b.N; i++ {
+		sort.Ints(els)
+	}
+}
+
+func TestSort(t *testing.T) {
+	a := makeElements(5)
+	Sort(a)
+	assert.NotNil(t, a)
+	assert.EqualValues(t, 5, len(a))
+	assert.EqualValues(t, 0, a[0])
+	assert.EqualValues(t, 1, a[1])
+	assert.EqualValues(t, 2, a[2])
+	assert.EqualValues(t, 3, a[3])
+	assert.EqualValues(t, 4, a[4])
+}
+
+func BenchmarkSort100(b *testing.B) {
+	els := makeElements(100)
+	for i := 0; i < b.N; i++ {
+		Sort(els)
+	}
+}
+func BenchmarkSort100000(b *testing.B) {
+	els := makeElements(100000)
+	for i := 0; i < b.N; i++ {
+		Sort(els)
+	}
+}
