21abbe417303eb24b0ef3db286205e74af2ebddf
agniadvani <advani.agni.agni@gmail.com>
2020-06-12 17:36:11 +0530

    first microservice

--- 01_mvc/services/users_service.go
+++ 01_mvc/services/users_service.go
@@ -1,8 +1,8 @@
 package services
 
 import (
-	"github.com/advaniagni/golang-microservices/01_mvc/domain"
-	"github.com/advaniagni/golang-microservices/01_mvc/utilis"
+	"github.com/agniadvani/golang-microservices/01_mvc/domain"
+	"github.com/agniadvani/golang-microservices/01_mvc/utilis"
 )
 
 func GetUser(userId int64) (*domain.User, *utilis.ApplicationError) {
